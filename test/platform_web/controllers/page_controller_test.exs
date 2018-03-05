ee19a6889fe83e0c162368d4448e3b47fbcc51d1
Igor Ladkin <igor.ladkin@xing.com>
2018-03-05 10:33:04 +0100

    Update broken test

--- test/platform_web/controllers/page_controller_test.exs
+++ test/platform_web/controllers/page_controller_test.exs
@@ -2,7 +2,7 @@ defmodule PlatformWeb.PageControllerTest do
   use PlatformWeb.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    conn = get(conn, "/")
+    assert html_response(conn, 200) =~ "Players"
   end
 end
